aad49999b035579bd16170d6e66650f4bb49202f
abrachet <alexbrachetmialot@gmail.com>
2019-05-22 02:02:49 -0400

    updated easy compiler test

--- c01_test.c
+++ c01_test.c
@@ -4,12 +4,14 @@
 
 char_a 65; /* A */
 
-main(argc, int argv[])
+main(argc, argv)
+int argv[];
 {
     extern putchar;
     extern char_a;
 
     putchar(char_a);
+    putchar('\n');
 
     return(0);
 }
